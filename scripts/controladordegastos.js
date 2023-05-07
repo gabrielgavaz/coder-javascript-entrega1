f9dc0b150f227193bd9854548b22af55ca889a5b
Gabriel gavaz <gabrielgavaz@gmail.com>
2023-05-06 22:45:58 -0300

    se ha insertado dom

--- scripts/controladordegastos.js
+++ scripts/controladordegastos.js
@@ -1,68 +1,132 @@
-let opcion;
-let id= 1;
-let gastoDiario = 0.0;
-let categorias = []; // definir un array vacío para almacenar los objetos de gastos
-let i = 0;
+let restante = 0;
 
-do {
-  // Mostramos el menú
-  opcion = parseInt(prompt("=== MENÚ DE GASTOS === \n 1. Añadir gasto diario\n 2. Ver gasto total\n 3. Mostrar las categorias de los gastos\n 4. Filtrar\n 5. Salir"));
-
-  switch (opcion) {
-    case 1:
-      // Pedimos al usuario que ingrese la categoria y el gasto diario
-      const nuevaCategoria = prompt("Ingrese categoría del gasto:");
-      const nuevoGasto = parseFloat(prompt("Ingrese el monto del gasto diario:"));
-      gastoDiario += nuevoGasto;
-      console.log(`Gasto diario de ${nuevoGasto} agregado correctamente.`);
-      const gasto = {
-        categoria: nuevaCategoria,
-        monto: nuevoGasto,
-        id: id,
-      };
-      categorias.push(gasto); // agregar el objeto de gasto al array categorias
-      id++;
-      break;
-
-    case 2:
-      // Mostramos el gasto total
-      console.log(`Gasto total: ${gastoDiario}`);
-      break;
-
-    case 3:
-      // Mostramos las categorias y los gastos
-      console.log("Categorías de gastos:");
-      for (let i = 0; i < categorias.length; i++) {
-        console.log(`categoria:${categorias[i].categoria} precio: ${categorias[i].monto} id: ${categorias[i].id}`);
-      }
-      break;
-
-    case 4:
-
-      // Filtramos los gastos por categoría
-      const categoriaFiltro = prompt("Ingrese la categoría que desea filtrar:");
-      const gastosFiltrados = categorias.filter((gasto) => gasto.categoria === categoriaFiltro);
-
-      if (gastosFiltrados.length > 0) {
-        console.log(`Gastos de la categoría ${categoriaFiltro}:`);
-        gastosFiltrados.forEach((gasto) => {
-          console.log(`- Monto: ${gasto.monto} (ID: ${gasto.id}) -`);
-        });
-      } else {
-        console.log(`No hay gastos registrados para la categoría ${categoriaFiltro}.`);
-      }
-
-    
-    break;
-
-    case 5:
-      // Salimos del programa
-      console.log("Saliendo del programa...");
-      break;
-
-    default:
-      // Si el usuario ingresa una opción no válida, mostramos un mensaje de error
-      console.log("Opción no válida. Por favor, seleccione una opción del menú.");
-      break;
+const guardarPresupuesto = () => {
+  let presupuesto = parseInt(document.querySelector("#presupuestoInicial").value);
+  restante = presupuesto;
+  if (presupuesto < 1 || isNaN(presupuesto)) {
+    mostrarError("#msj_error_pregunta", "Cantidad No Válida");
+    return;
   }
-} while (opcion !== 5);
+  localStorage.setItem("presupuesto", presupuesto);
+  localStorage.setItem("gastos", JSON.stringify([]));
+
+  actualizarVista();
+}
+
+const actualizarVista = () => {
+  let presupuesto = localStorage.getItem("presupuesto");
+
+  let divPregunta = document.querySelector("#pregunta");
+  let divGastos = document.querySelector("#divGastos");
+  let divControlGastos = document.querySelector("#divControlGastos");
+
+  divPregunta.style.display = "none";
+  divGastos.style.display = "none";
+
+
+  let controlGastos = `<div class="gastos-realizados">
+                      <h2>Listado de Gastos</h2>
+                      <div class="alert alert-success">
+                      Presupuesto:$ ${presupuesto}</div>
+                      <div class="alert alert-success">
+                      Restante:$ ${presupuesto}</div>
+                    </div>`;
+
+  if (!presupuesto) {
+    divPregunta.style.display = "block";
+  } else {
+    divPregunta.style.display = "none";
+    divGastos.style.display = "flex";
+    divControlGastos.innerHTML = controlGastos;
+    refrescarListado();
+  }
+}
+
+const agregarGasto = () => {
+  let tipoGasto = document.querySelector("#tipoGasto").value;
+  let cantidadGasto = parseInt(document.querySelector("#cantidadGasto").value);
+
+  if (cantidadGasto < 1 || isNaN(cantidadGasto) || tipoGasto.trim() === '' || !/^[a-zA-Z]+$/.test(tipoGasto)) {
+    mostrarError("#msj_error_crearGasto", "ERROR EN CAMPOS");
+    return;
+  }
+
+  let presupuesto = parseInt(localStorage.getItem("presupuesto"));
+
+  if (cantidadGasto > restante) {
+    mostrarError("#msj_error_crearGasto", "Cantidad mayor al restante");
+    return;
+  }
+  let nuevoGasto = {
+    tipoGasto,
+    cantidadGasto
+  }
+
+  let gastos = JSON.parse(localStorage.getItem("gastos"));
+  gastos.push(nuevoGasto);
+  localStorage.setItem("gastos", JSON.stringify(gastos));
+
+  restante = presupuesto - cantidadGasto; // Actualizar el valor restante
+  refrescarListado(); // Actualizar la vista del listado de gastos
+
+  document.querySelector("#formGasto").reset();
+}
+
+const refrescarListado = () => {
+  let presupuesto = parseInt(localStorage.getItem("presupuesto"));
+  let gastos = JSON.parse(localStorage.getItem("gastos"));
+
+  let totalGastos = 0;
+  let listadoHTML = ``;
+  gastos.map(gasto => {
+    listadoHTML += `<li class="gastos">
+                <p class="parrafoTipoGasto">${gasto.tipoGasto}</p>
+                <p class="parrafoCantidadGasto">$ ${gasto.cantidadGasto}</p>
+                </li>`;
+    totalGastos += parseInt(gasto.cantidadGasto);
+  });
+  console.log("total de gastos: " + totalGastos);
+
+  restante = presupuesto - totalGastos;
+
+  let divControlGastos = document.querySelector("#divControlGastos");
+
+  divControlGastos.innerHTML = ``;
+
+  let clase = "";
+  
+  if ((presupuesto / 4) > restante) {
+    clase = "alerta alerta-rojo";
+  } else if ((presupuesto / 2) > restante) {
+    clase = "alerta alerta-naranja";
+  } else {
+    clase = "alert alerta-verde";
+  }
+
+  divControlGastos.innerHTML = `<div class="gastos-realizados">
+                              <h2>Listado de gastos</h2>
+                              ${listadoHTML}
+                              <div class="alert alert-success">
+                              Presupuesto: $ ${presupuesto}</div>
+                              <div class="${clase}">
+                              Restante: $ ${restante}</div>
+
+                              <button onclick="reiniciarPresupuesto()" class="botonPrincipal mr-top full-width">
+                              Reiniciar presupuesto</button>
+                            </div>`;
+}
+
+
+const reiniciarPresupuesto=()=>{
+  localStorage.clear();
+  location.reload(true);
+}
+
+const mostrarError = (elemento, mensaje) => {
+  let divError = document.querySelector(elemento);
+  divError.innerHTML = `<p class="alerta-error">${mensaje}</p>`;
+  setTimeout(() => {
+    divError.innerHTML = "";
+  }, 2000);
+}
+
